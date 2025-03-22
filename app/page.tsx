2811114150e76d7773c4f950f6ce2375c09d68ff
Cho Sungjun <chosungjun@Choui-MacBookPro.local>
2025-03-23 07:11:19 +0800

    Merge remote changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 // import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
@@ -19,8 +18,6 @@ const Home = () => {
         <Hero />
         <Grid />
         <RecentProjects />
-        {/* <Clients /> */}
-        {/* <Experience /> */}
         <Approach />
         <Footer />
       </div>
