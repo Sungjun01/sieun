5535ce3183651157e154e763bf7ca82464cb5cbf
Cho Sungjun <chosungjun@Choui-MacBookPro.local>
2025-03-23 06:51:08 +0800

    Merge remote changes

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -74,7 +74,6 @@ export const BentoGridItem = ({
         className
       )}
       style={{
-        background: "rgb(4,7,29)",
         background:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
